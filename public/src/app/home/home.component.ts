4c522aaccc6f9e3eae0af224fe64311d03eb9d04
kyle-mcr <kylemcreynolds92@gmail.com>
2021-03-07 20:46:50 -0800

    make mobile repsonsive

--- public/src/app/home/home.component.ts
+++ public/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var jQuery:any;
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+  }
+  ngAfterViewInit(){
+  (function ($) {
+    $(document).ready(function () {
+      $('.read_more').click(function(){
+        $('.content, .read_more').hide('slow');
+        $('.content2').show('slow');
+    });
+    $('.read_more2').click(function(){
+      $('.content3, .read_more2').hide('slow');
+      $('.content4').show('slow');
+  });
+    });
+    $('#orange_button').click(function(){
+      $('#orange_button').hide(500, "linear");
+      $('.content5').show(500, "linear");
+  });
+  $('#orange_button2').click(function(){
+    $('#orange_button2, .content6').hide(400, "linear");
+    $('.content7').show(400, "linear");
+});
+  })(jQuery);
+  }
+
+  goToDestination(id){
+    document.getElementById(id).scrollIntoView({behavior:'smooth'});
   }
 
 }
